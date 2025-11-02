dae9ff688e78b8e04547275313df3190a4f6f69c
Vivek Mishra <vivekmishra161993@gmail.com>
2025-11-02 13:57:32 +0530

    deleted unused files

--- app/src/main/java/com/mtt/jaapmala/util/SettingsKey.kt
+++
@@ -1,9 +0,0 @@
-package com.mtt.jaapmala.util
-
-import androidx.datastore.preferences.core.booleanPreferencesKey
-import androidx.datastore.preferences.core.stringPreferencesKey
-
-object SettingsKeys {
-    val REMINDER_OPTION = stringPreferencesKey("reminder_option")
-    val MEDITATION_SOUND = booleanPreferencesKey("meditation_sound")
-}
--- app/src/main/java/com/mtt/jaapmala/util/UIEvent.kt
+++
@@ -1,5 +0,0 @@
-package com.mtt.jaapmala.util
-
-sealed class UIEvent {
-    object TriggerFeedback : UIEvent()
-}
